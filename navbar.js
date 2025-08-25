7fbdc5cc9ca379b1e3c25def7f098696d20e564d
edgcht <edouard.gcht@gmail.col>
2025-08-25 16:14:59 +0200

    edit navbar #1

--- navbar.js
+++ navbar.js
@@ -1,2 +1,10 @@
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+function stickynavbar() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+    }
+    
